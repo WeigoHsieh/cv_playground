fb691aacac0d25d85756dbeae520df966cbe6752
unknown <09080381@gojal.com.tw>
2020-08-06 18:02:06 +0800

    備份所有測試題目

--- index.py
+++ index.py
@@ -72,6 +72,10 @@ class ImagePretreatmenter:
         in_block = 255 - dice_blocks [1]
         return in_block
         
+    def otsu(self,img):
+        blur = self.GaussianBlur(img)
+        _,th = cv.threshold(blur,0,255,cv.THRESH_BINARY + cv.THRESH_OTSU)
+        return th
     #自適應高斯模糊濾波器    
     def adaptiveGaussianThresHolding(self,img):
         return cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY, 11, 2)
@@ -97,7 +101,7 @@ class ImagePretreatmenter:
     
     #高斯模糊
     def GaussianBlur(self,img):
-        return cv.GaussianBlur(img,(9,9),0)
+        return cv.GaussianBlur(img,(5,5),0)
     
     # 拉普拉斯算子
     def lapl(self,img):
@@ -152,8 +156,10 @@ class ImagePretreatmenter:
          # gray = self.gray(i)    
          # sobel = self.sobel(gray)
          # contours = self.contours(i)
-         dice_blocks = self.diceblock(i)
-         yield dice_blocks
+         # dice_blocks = self.diceblock(i)
+         gray = self.gray(i)
+         otsu = self.otsu(gray)
+         yield otsu
     
     #圖片裁減   
     def cut(self, img,x,y,w,h):
