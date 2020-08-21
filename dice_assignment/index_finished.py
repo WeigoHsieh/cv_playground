706a67efb82114479e64571b212b929a567aef62
unknown <09080381@gojal.com.tw>
2020-08-21 10:50:58 +0800

    微調整案件

--- dice_assignment/index_finished.py
+++ dice_assignment/index_finished.py
@@ -34,14 +34,14 @@ class VideoCapturer:
             if _:
                 cv.imshow('Original Camera in Camera: No.' + str(self.camera), frame)
                 k = cv.waitKey(1)
-                # if k == ord('s'):
-                #     self.cannyP = can_high
-                #     self.cluster = 3
-                #     cv.imshow('w', frame)
-                #     print('鏡頭拍攝(camera on)')
-                #     self.frames.append(frame)
-                #     # self.download(frame)
-                if k == ord('1'):
+                if k == ord('e'):
+                    self.cannyP = can_low
+                    self.cluster = 4
+                    cv.imshow('w', frame)
+                    print('鏡頭拍攝(camera on)')
+                    self.frames.append(frame)
+                    # self.download(frame)
+                elif k == ord('1'):
                     self.cannyP = can_low
                     self.cluster = 1
                     cv.imshow('w', frame)
@@ -63,40 +63,13 @@ class VideoCapturer:
                     print('鏡頭拍攝(camera on)')
                     self.frames.append(frame)
                     # self.download(frame)
-                
-                elif k == ord('a'):
-                    self.cluster = 1
-                    self.cannyP = can_high
+                elif k == ord('4'):
+                    self.cannyP = 20
+                    self.cluster = 3
                     cv.imshow('w', frame)
                     print('鏡頭拍攝(camera on)')
                     self.frames.append(frame)
-                    # self.download(frame)
-                elif k == ord('s'):
-                     self.cannyP = can_high
-                     self.cluster = 2
-                     cv.imshow('w', frame)
-                     print('鏡頭拍攝(camera on)')
-                     self.frames.append(frame)
-                elif k == ord('d'):
-                     self.cluster = 3
-                     self.cannyP = can_high
-                     cv.imshow('w', frame)
-                     print('鏡頭拍攝(camera on)')
-                     self.frames.append(frame)
-                elif k == ord('w'):
-                     self.cluster = 5
-                     self.cannyP = can_high
-                     cv.imshow('w', frame)
-                     print('鏡頭拍攝(camera on)')
-                     self.frames.append(frame)
-                elif k == ord('e'):
-                     self.cluster = 5
-                     self.cannyP = can_low
-                     cv.imshow('w', frame)
-                     print('鏡頭拍攝(camera on)')
-                     self.frames.append(frame)
-                elif k == 32:
-                     cv.imwrite('temp123.png',frame)
+                    # self.download(frame)    
                 elif k == ord('q'):
                     print('鏡頭退出(camera exit)')
                     break
@@ -127,57 +100,7 @@ class ImagePretreatmenter:
             cv.imshow('showq',cl1)
         return cl1
     
-    def sobel(self,img):
-        img = cv.blur(img,(5,5))
-        img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-        x = cv.Sobel(img,cv.CV_16S,1,0)
-        y = cv.Sobel(img,cv.CV_16S,0,1)
-        absX = cv.convertScaleAbs(x)         
-        absY = cv.convertScaleAbs(y)
-        dst = cv.addWeighted(absX,0.5,absY,0.5,0)
-        return dst
-
-    
-    def blob(self,img):
-        gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-        # threshold = self.adaptive(gray)
-        detector = cv.SimpleBlobDetector()
-        keypoints = detector.detect(gray)
-        im_with_keypoints = cv.drawKeypoints(gray,
-                                             keypoints,
-                                             np.array([]),
-                                             (0,0,255),
-                                             cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        return im_with_keypoints
-    
-    def adaptive(self,img):
-        gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-        gray = self.medianBlur(gray)
-        img = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv.THRESH_BINARY,11,2)
-        return img
-    
-    def gamma_trans(self,img,gamma):
-	#具體做法先歸一化到1，然後gamma作為指數值求出新的畫素值再還原
-    	gamma_table = [np.power(x/255.0,gamma)*255.0 for x in range(256)]
-    	gamma_table = np.round(np.array(gamma_table)).astype(np.uint8)
-    	#實現對映用的是Opencv的查表函式
-    	return cv.LUT(img,gamma_table)
-
-    def updateAlpha(self,x):
-        alpha = 0.3
-        alpha = cv.getTrackbarPos('Alpha','image')
-        alpha = alpha * 0.01
-        self._img_list[0] = np.uint8(np.clip((alpha * self._img_list[0] + 80), 0, 255))    
-        
-        
-    def is_brightness(self,gray):
-        gray = Image.fromarray(gray)
-        stat = ImageStat.Stat(gray)
-        if stat.rms[0]> 100:
-            return True
-        return False
-    
+  
         
     def gaussianBlur(self,img_or_gray):
         return cv.GaussianBlur(img_or_gray, (7, 7), 0)
@@ -197,11 +120,10 @@ class ImagePretreatmenter:
     def houghCirlce(self,img):
         
         img = self.cli(img)
-        test_print(self.is_brightness(img))
-        canny = self.canny(img, self.cannyP)
+        canny = self.canny(self.ex(img), self.cannyP)
         
         if(testing == True):
-            cv.imshow('cany',(canny-25)*100)
+            cv.imshow('cany',(canny))
         # if(self.tempFrame == 0): #!!!
         cnt = cv.HoughCircles(canny, cv.HOUGH_GRADIENT, 1, 18, param1=12, #!!!
                       param2=15, minRadius=8, maxRadius=16) #!!!
@@ -214,19 +136,21 @@ class ImagePretreatmenter:
         for i in self._img_list:
             cnt = self.houghCirlce(i)
             self.cnt.append(cnt)
+            
     def start(self):
         self.processing()
+        
 class PatternMatcher:
     def __init__(self,img_pretreatmenter):
         self.R = 0
         self.img_pretreatmenter = img_pretreatmenter
         self.start_time = self.img_pretreatmenter.start_time
-        self.end_time = time.process_time()
-        self.finish_time = 'Process Time:' + str((self.end_time - self.start_time) * 1000) + 'ms'
         self.cnt = img_pretreatmenter.cnt 
         self._img_list = img_pretreatmenter._img_list
         self.cluster = img_pretreatmenter.cluster
         self.start()
+       
+        
     def kmeans(self,data,cluster):
         # if (len(data) == 2):
         #     cluster = 2
@@ -243,7 +167,8 @@ class PatternMatcher:
         result = km.fit_predict(data)
         result = np.sort(result)
         test_print(result)
-        return result                
+        return result  
+              
     def draw_pips(self,img):
         test_print(self.cnt[0])
         if(self.cnt[0] is None):
@@ -266,8 +191,11 @@ class PatternMatcher:
             cv.imshow(str(total), img)
        
     def draw_processing_time(self,img):
+        self.end_time = time.process_time()
+        self.finish_time = 'Process Time:' + str((self.end_time - self.start_time) * 1000) + 'ms'
         cv.putText(img,self.finish_time,(10,30),cv.FONT_HERSHEY_COMPLEX,0.6,(0,255,0))
         return img
+    
     def show_dices_and_pips(self):
         if(self.cluster <= 3):
             res = self.kmeans(self.cnt[0][0], self.cluster)
@@ -307,10 +235,8 @@ class PatternMatcher:
                 cv.destroyAllWindows()
             cv.waitKey(0)
             cv.destroyAllWindows()
-    
-    
 if __name__ == '__main__':
-    testing = False
+    testing = True
     video_capturer = VideoCapturer(0)
     image_pretreatmenter = ImagePretreatmenter(video_capturer,video_capturer.frames)
     pattern_matcher = PatternMatcher(image_pretreatmenter)
